b763bc39622e57cf8df5190df67c334ad14e57bc
juri <ojl98@naver.com>
2024-03-23 23:03:37 +0900

    ReplyMapperTests : 댓글 추가 테스트 완료

--- src/test/java/org/zerock/persistence/ReplyMapperTests.java
+++ src/test/java/org/zerock/persistence/ReplyMapperTests.java
@@ -1,10 +1,13 @@
 package org.zerock.persistence;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.ReplyVO;
 import org.zerock.mapper.ReplyMapper;
 
 import lombok.extern.log4j.Log4j;
@@ -14,8 +17,24 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ReplyMapperTests {
   
+	private Long[] bnoArr = {1029L, 1028L, 1027L, 1025L, 1024L};
+	
   @Autowired
   private ReplyMapper mapper;
+  
+  @Test
+  public void testCreate() {
+	  IntStream.rangeClosed(1,10).forEach(i->{
+		  ReplyVO vo = new ReplyVO();
+		  
+		  //게시물의 번호
+		  vo.setBno(bnoArr[i % 5]);
+		  vo.setReply("댓글테스트 " + i);
+		  vo.setReplyer("replyer"+i);
+		  
+		  mapper.insert(vo);
+	  });
+  }
   
   @Test
   public void testMapper() {
