604e0916621bce083f0dcd4ab476f7ddd804e11e
오주리 <ojl98@naver.com>
2024-03-24 01:18:13 +0900

    ReplyController에서 게시글 댓글 가져오기

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -1,12 +1,17 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -35,5 +40,19 @@ public class ReplyController {
 				? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		//삼항연산자
+	}
+	
+	@GetMapping(value="/pages/{bno}/{page}",
+			produces= {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno){
+		log.info("getList....................");
+		Criteria cri = new Criteria(page, 10);
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno),HttpStatus.OK);
 	}
 }
